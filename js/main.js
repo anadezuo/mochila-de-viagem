e0d255659f423a5cafbc09045492181cc58499a7
Ana Paula Dezuo <ana.dezuo@rdstation.com>
2023-05-21 01:31:33 -0300

    chore: update item localStorage

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+const AMOUNT = 'Quantidade'
+const NAME = 'Nome'
+
 const form = document.getElementById('formNovoItem')
+
+restoreItensListFromLocalStorage()
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
@@ -6,15 +11,26 @@ form.addEventListener('submit', (event) => {
   const amount = event.target.elements['amount'];
 
   if (!name.value) return;
+  if (!amount.value) amount.value = 0;
 
-  const itemElement = createItemElement({name: name.value, amount: amount.value})
-  insertItemElementInList(itemElement)
+  const newItem = {name: name.value, amount: amount.value}
   const items = getItemsLocalStorage()
-  saveItemLocalStorage(items, {name: name.value, amount: amount.value})
+
+
+  if (hasItemLocalStorage(items, name.value)) {
+    updateItemLocalStorage(items, newItem)
+  }
+  else {
+    const itemElement = createItemElement(newItem)
+    insertItemElementInList(itemElement)
+    saveItemLocalStorage(items, {name: name.value, amount: amount.value})
+  }
+
   handleInputClear(name, amount)
 })
 
-function createItemElement({name, amount}){
+
+function createItemElement({name, amount}) {
   const strongElement = document.createElement('strong');
   strongElement.innerHTML = amount;
 
@@ -26,25 +42,54 @@ function createItemElement({name, amount}){
   return itemElement;
 }
 
-function insertItemElementInList(itemElement){
+function insertItemElementInList(itemElement) {
   const list = document.getElementById('list')
   list.appendChild(itemElement)
 }
 
-function handleInputClear(name, amount){
+function handleInputClear(name, amount) {
   name.value = ""
   amount.value = ""
 }
 
-function getItemsLocalStorage(){
+function getItemsLocalStorage() {
   return localStorage.getItem("lista") ? JSON.parse(localStorage.getItem("lista")) : []
 }
 
-function saveItemLocalStorage( items, {name, amount}){
+function hasItemLocalStorage(items, value) {
+  return items.find(item => item[NAME] === value)
+}
+
+function saveItemLocalStorage( items, {name, amount}) {
   const newItem = {
-    "Nome": name,
-    "Quantidade": amount
+    [NAME]: name,
+    [AMOUNT]: amount
   }
   items.push(newItem)
+  setItemsLocalStorage(items)
+}
+
+function updateItemLocalStorage(items, newItem) {
+  const newItems = items.map(item => {
+    if (item[NAME] === newItem.name)
+      item[AMOUNT] = parseInt(item[AMOUNT]) + parseInt(newItem.amount)
+    
+    return item
+  })
+  setItemsLocalStorage(newItems)
+}
+
+function setItemsLocalStorage(items) {
+  //* o localStorage aceita apenas string
+  //* dados sensiveis devem ser armazenados nos cookies e criptografados
   localStorage.setItem("lista" , JSON.stringify(items))
+}
+
+function restoreItensListFromLocalStorage() {
+  const items = getItemsLocalStorage()
+
+  items.forEach(item => {
+    const itemElement = createItemElement({name: item[NAME], amount: item[AMOUNT]})
+    insertItemElementInList(itemElement)
+  })
 }
